0baf628d9c2cca0cf692246085b411d089f74098
shivam <shivam@coderootz.com>
2021-09-23 12:58:43 +0530

    set sequence initial code done

--- src/setsequence.ts
+++ src/setsequence.ts
@@ -1,3 +1,16 @@
-export function setsequence(objArray: any[]) {
-    return 'success';
+export function setsequence(requiredSeq: any[], seqIget: any[], searchKey: any) {
+    var finalResult: any[]=[];
+    var j=0;
+    for(j; j<requiredSeq.length; j++)
+    {
+        var i=0;
+        for(i;i<requiredSeq.length; i++)
+        {
+            if(requiredSeq[j][searchKey] == seqIget[i][searchKey])
+            {
+                finalResult[j] = seqIget[i];
+            }
+        }
+    }
+    return finalResult;
 }
--- src/tests/Initial.test.ts
+++ src/tests/Initial.test.ts
@@ -1,5 +1,37 @@
 import { setsequence } from '../setsequence';
 
 test('Set sequence', () => {
-    expect(setsequence([{}])).toBe('success');
-  });
+  var initial: any[]=[
+    {'num':1},
+    {'num':2},
+    {'num':3},
+    {'num':4},
+    {'num':5},
+    {'num':6},
+    {'num':7}
+  ];
+  var final: any[]=[
+    {'num':4},
+    {'num':2},
+    {'num':3},
+    {'num':1},
+    {'num':7},
+    {'num':6},
+    {'num':5}
+  ];
+
+  // var result1:any[] = [1,2,3,4,5,6,7]; 
+  //   expect(setsequence(initial,final,'num')).toBe(result1);
+  // });
+
+  var result1:any[] = [
+    {'num':1},
+    {'num':2},
+    {'num':3},
+    {'num':4},
+    {'num':5},
+    {'num':6},
+    {'num':7}
+  ];; 
+  expect(setsequence(initial,final,'num')).toStrictEqual(result1);
+});
