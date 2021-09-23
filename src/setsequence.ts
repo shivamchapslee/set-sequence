9b7062a5c3551b1a424c9d1760971db7e0ca274f
shivam <shivam@coderootz.com>
2021-09-23 13:15:42 +0530

    removed bad programming practices

--- src/setsequence.ts
+++ src/setsequence.ts
@@ -1,16 +1,13 @@
 export function setsequence(requiredSeq: any[], seqIget: any[], searchKey: any) {
-    const finalResult: any[]=[];
-    let j=0;
-    for(j; j<requiredSeq.length; j++)
-    {
-        let i=0;
-        for(i;i<requiredSeq.length; i++)
-        {
-            if(requiredSeq[j][searchKey] === seqIget[i][searchKey])
-            {
-                finalResult[j] = seqIget[i];
-            }
-        }
+  const finalResult: any[] = [];
+  let j = 0;
+  for (j; j < requiredSeq.length; j++) {
+    let i = 0;
+    for (i; i < requiredSeq.length; i++) {
+      if (requiredSeq[j][searchKey] === seqIget[i][searchKey]) {
+        finalResult[j] = seqIget[i];
+      }
     }
-    return finalResult;
+  }
+  return finalResult;
 }
--- src/tests/Initial.test.ts
+++ src/tests/Initial.test.ts
@@ -1,37 +1,13 @@
 import { setsequence } from '../setsequence';
 
 test('Set sequence', () => {
-  const initial: any[]=[
-    {'num':1},
-    {'num':2},
-    {'num':3},
-    {'num':4},
-    {'num':5},
-    {'num':6},
-    {'num':7}
-  ];
-  const final: any[]=[
-    {'num':4},
-    {'num':2},
-    {'num':3},
-    {'num':1},
-    {'num':7},
-    {'num':6},
-    {'num':5}
-  ];
-  const result1:any[] = [
-    {'num':1},
-    {'num':2},
-    {'num':3},
-    {'num':4},
-    {'num':5},
-    {'num':6},
-    {'num':7}
-  ];
+  const initial: any[] = [{ num: 1 }, { num: 2 }, { num: 3 }, { num: 4 }, { num: 5 }, { num: 6 }, { num: 7 }];
+  const final: any[] = [{ num: 4 }, { num: 2 }, { num: 3 }, { num: 1 }, { num: 7 }, { num: 6 }, { num: 5 }];
+  const result1: any[] = [{ num: 1 }, { num: 2 }, { num: 3 }, { num: 4 }, { num: 5 }, { num: 6 }, { num: 7 }];
 
-  // var result1:any[] = [1,2,3,4,5,6,7]; 
+  // var result1:any[] = [1,2,3,4,5,6,7];
   //   expect(setsequence(initial,final,'num')).toBe(result1);
   // });
 
-  expect(setsequence(initial,final,'num')).toStrictEqual(result1);
+  expect(setsequence(initial, final, 'num')).toStrictEqual(result1);
 });
