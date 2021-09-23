3a129bfb73c3989764878f0920f3848fa9a571af
shivam <shivam@coderootz.com>
2021-09-23 13:03:09 +0530

    removed bad programming prachices

--- src/setsequence.ts
+++ src/setsequence.ts
@@ -1,12 +1,12 @@
 export function setsequence(requiredSeq: any[], seqIget: any[], searchKey: any) {
-    var finalResult: any[]=[];
-    var j=0;
+    const finalResult: any[]=[];
+    let j=0;
     for(j; j<requiredSeq.length; j++)
     {
-        var i=0;
+        let i=0;
         for(i;i<requiredSeq.length; i++)
         {
-            if(requiredSeq[j][searchKey] == seqIget[i][searchKey])
+            if(requiredSeq[j][searchKey] === seqIget[i][searchKey])
             {
                 finalResult[j] = seqIget[i];
             }
