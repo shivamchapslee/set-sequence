d8af361e882010ad7774aafcdc51b18f9eda9d53
shivam <shivam@coderootz.com>
2021-09-23 13:04:59 +0530

    removed bad programming practices

--- src/tests/Initial.test.ts
+++ src/tests/Initial.test.ts
@@ -1,7 +1,7 @@
 import { setsequence } from '../setsequence';
 
 test('Set sequence', () => {
-  var initial: any[]=[
+  const initial: any[]=[
     {'num':1},
     {'num':2},
     {'num':3},
@@ -10,7 +10,7 @@ test('Set sequence', () => {
     {'num':6},
     {'num':7}
   ];
-  var final: any[]=[
+  const final: any[]=[
     {'num':4},
     {'num':2},
     {'num':3},
@@ -19,12 +19,7 @@ test('Set sequence', () => {
     {'num':6},
     {'num':5}
   ];
-
-  // var result1:any[] = [1,2,3,4,5,6,7]; 
-  //   expect(setsequence(initial,final,'num')).toBe(result1);
-  // });
-
-  var result1:any[] = [
+  const result1:any[] = [
     {'num':1},
     {'num':2},
     {'num':3},
@@ -32,6 +27,11 @@ test('Set sequence', () => {
     {'num':5},
     {'num':6},
     {'num':7}
-  ];; 
+  ];
+
+  // var result1:any[] = [1,2,3,4,5,6,7]; 
+  //   expect(setsequence(initial,final,'num')).toBe(result1);
+  // });
+
   expect(setsequence(initial,final,'num')).toStrictEqual(result1);
 });
